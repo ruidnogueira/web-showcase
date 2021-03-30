c8859549fb521ba28e7ad2ec2bbd0c3bbf4c8801
Rui Nogueira <ruidnogueira@outlook.com>
2021-03-31 00:31:14 +0100

    chore: setup storybook and chromatic

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+// import '../src/styles/styles.scss';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  viewport: {
+    viewports: {
+      galaxyS9: {
+        name: 'Galaxy S9/S9+',
+        styles: {
+          width: '360px',
+          height: '740px',
+        },
+      },
+    },
+  },
+};
