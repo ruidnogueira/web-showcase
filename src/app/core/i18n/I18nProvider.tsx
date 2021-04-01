f075b64574c637e9e7a2dacc6c993abb1cabcb6e
Rui Nogueira <ruidnogueira@outlook.com>
2021-04-01 01:22:58 +0100

    chore: create i18n instance only once

--- src/app/core/i18n/I18nProvider.tsx
+++ src/app/core/i18n/I18nProvider.tsx
@@ -1,5 +1,5 @@
-import { ReactNode, useEffect, useMemo } from 'react';
-import i18next, { InitOptions } from 'i18next';
+import { ReactNode, useEffect } from 'react';
+import i18next from 'i18next';
 import { I18nextProvider } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
@@ -10,29 +10,28 @@ import { getI18nFormatter } from './i18nFormat';
 export function I18nProvider({ children }: { children: ReactNode }) {
   const { i18nConfig } = useConfig();
 
-  const i18n = useMemo(() => {
-    const i18nInitOptions: InitOptions = {
-      fallbackLng: i18nConfig.fallbackLanguage,
-      debug: process.env.NODE_ENV === 'development',
-      detection: {
-        order: ['localStorage', 'navigator', 'path'],
-        lookupLocalStorage: i18nConfig.localStorageKey,
-      },
-      backend: {
-        loadPath: `${process.env.PUBLIC_URL}/locales/{{lng}}/{{ns}}.json`,
-      },
-      interpolation: {
-        escapeValue: false, // not needed for react as it escapes by default
-        format: getI18nFormatter(i18nConfig),
-      },
-    };
-
-    return i18next.createInstance(i18nInitOptions);
-  }, [i18nConfig]);
-
   useEffect(() => {
-    i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init();
-  }, [i18n]);
+    i18next
+      .use(Backend)
+      .use(LanguageDetector)
+      .use(initReactI18next)
+      .init({
+        fallbackLng: i18nConfig.fallbackLanguage,
+        debug: process.env.NODE_ENV === 'development',
+        detection: {
+          order: ['localStorage', 'navigator', 'path'],
+          lookupLocalStorage: i18nConfig.localStorageKey,
+        },
+        backend: {
+          loadPath: `${process.env.PUBLIC_URL}/locales/{{lng}}/{{ns}}.json`,
+        },
+        interpolation: {
+          escapeValue: false, // not needed for react as it escapes by default
+          format: getI18nFormatter(i18nConfig),
+        },
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
+  return <I18nextProvider i18n={i18next}>{children}</I18nextProvider>;
 }
