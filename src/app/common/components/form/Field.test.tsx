f0168a9753d42aa06e167f8a74de3bf19647a2cc
Rui Nogueira <ruidnogueira@outlook.com>
2021-04-16 01:44:40 +0100

    feat: add label and field components

---
+++ src/app/common/components/form/Field.test.tsx
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import { Field } from './Field';
+
+test('renders', () => {
+  render(<Field>field text</Field>);
+});
---
+++ src/app/common/components/form/Label.test.tsx
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import { Label } from './Label';
+
+test('renders', () => {
+  render(<Label>label text</Label>);
+});
