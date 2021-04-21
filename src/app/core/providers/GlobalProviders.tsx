2b1b5d42443ed3b943c8614b1f1ed0edd947a7ab
Rui Nogueira <ruidnogueira@outlook.com>
2021-04-21 22:45:51 +0100

    feat: add auth machine and provider

--- src/app/core/providers/GlobalProviders.tsx
+++ src/app/core/providers/GlobalProviders.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
+import { AuthenticationMachineProvider } from '../auth/AuthenticationMachineProvider';
 import { ThemeProvider } from './ThemeProvider';
 
 export function GlobalProviders({ children }: { children: ReactNode }) {
   return (
     <HelmetProvider>
-      <ThemeProvider>{children}</ThemeProvider>
+      <ThemeProvider>
+        <AuthenticationMachineProvider>{children}</AuthenticationMachineProvider>
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
