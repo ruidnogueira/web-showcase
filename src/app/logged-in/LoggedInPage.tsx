36e9220801020e8c7c667d81c8a454a8f2055bdd
Rui Nogueira <ruidnogueira@outlook.com>
2021-04-26 12:08:11 +0100

    feat: add logout to loggedin page

--- src/app/logged-in/LoggedInPage.tsx
+++ src/app/logged-in/LoggedInPage.tsx
@@ -1,3 +1,21 @@
+import { Button } from 'app/common/components/button/Button';
+import { AuthMachineEventType } from 'app/core/auth/authMachine';
+import { useAuthMachine } from 'app/core/auth/AuthMachineProvider';
+import { MouseEvent } from 'react';
+
 export function LoggedInPage() {
-  return <div>LoggedInPage works!</div>;
+  const [, sendAuthEvent] = useAuthMachine();
+
+  const handleLogout = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    sendAuthEvent({ type: AuthMachineEventType.Logout });
+  };
+
+  return (
+    <div>
+      <Button type="button" onClick={handleLogout}>
+        Logout
+      </Button>
+    </div>
+  );
 }
