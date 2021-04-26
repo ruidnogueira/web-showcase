0a19b8885965a598e804021e8e5b5f457528ecfb
Rui Nogueira <ruidnogueira@outlook.com>
2021-04-26 01:33:29 +0100

    fix: wait for msw service worker before rendering

--- src/index.tsx
+++ src/index.tsx
@@ -10,39 +10,43 @@ import { I18nProvider } from 'app/core/i18n/I18nProvider';
 import { worker } from 'mocks/server/browser.mock';
 import { GlobalProviders } from 'app/core/providers/GlobalProviders';
 
-if (window.location.pathname === process.env.PUBLIC_URL) {
-  // https://github.com/w3c/ServiceWorker/issues/1468
-  // due to a problem with service worker implementation when served from subdirectories,
-  // setting react homepage property in package.json causes service workers to fail when not using trailing slash
-  // does't work -> http://localhost:3000/example
-  // works -> http://localhost:3000/example/
-  window.location.pathname = process.env.PUBLIC_URL + '/';
+async function render() {
+  if (window.location.pathname === process.env.PUBLIC_URL) {
+    // https://github.com/w3c/ServiceWorker/issues/1468
+    // due to a problem with service worker implementation when served from subdirectories,
+    // setting react homepage property in package.json causes service workers to fail when not using trailing slash
+    // does't work -> http://localhost:3000/example
+    // works -> http://localhost:3000/example/
+    window.location.pathname = process.env.PUBLIC_URL + '/';
+  }
+
+  await worker.start({
+    serviceWorker: {
+      url: `${process.env.PUBLIC_URL}/mockServiceWorker.js`,
+    },
+  });
+
+  ReactDOM.render(
+    <StrictMode>
+      <Profiler id="App" onRender={() => {}}>
+        <BrowserRouter>
+          <ConfigProvider>
+            <I18nProvider>
+              <GlobalProviders>
+                <Suspense fallback={<div>Loading...</div>}>
+                  <App />
+                </Suspense>
+              </GlobalProviders>
+            </I18nProvider>
+          </ConfigProvider>
+        </BrowserRouter>
+      </Profiler>
+    </StrictMode>,
+    document.getElementById('root')
+  );
 }
 
-worker.start({
-  serviceWorker: {
-    url: `${process.env.PUBLIC_URL}/mockServiceWorker.js`,
-  },
-});
-
-ReactDOM.render(
-  <StrictMode>
-    <Profiler id="App" onRender={() => {}}>
-      <BrowserRouter>
-        <ConfigProvider>
-          <I18nProvider>
-            <GlobalProviders>
-              <Suspense fallback={<div>Loading...</div>}>
-                <App />
-              </Suspense>
-            </GlobalProviders>
-          </I18nProvider>
-        </ConfigProvider>
-      </BrowserRouter>
-    </Profiler>
-  </StrictMode>,
-  document.getElementById('root')
-);
+render();
 
 // TODO ADD service worker check for updates
 serviceWorkerRegistration.register();
