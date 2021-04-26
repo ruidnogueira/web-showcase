ca37ac6d9fd36002e3924b2c539f4363a24561a1
Rui Nogueira <ruidnogueira@outlook.com>
2021-04-26 01:06:18 +0100

    fix: service worker trailing slash

--- src/index.tsx
+++ src/index.tsx
@@ -10,9 +10,19 @@ import { I18nProvider } from 'app/core/i18n/I18nProvider';
 import { worker } from 'mocks/server/browser.mock';
 import { GlobalProviders } from 'app/core/providers/GlobalProviders';
 
+if (window.location.pathname === process.env.PUBLIC_URL) {
+  // https://github.com/w3c/ServiceWorker/issues/1468
+  // due to a problem with service worker implementation when served from subdirectories,
+  // setting react homepage property in package.json causes service workers to fail when not using trailing slash
+  // does't work -> http://localhost:3000/example
+  // works -> http://localhost:3000/example/
+  window.location.pathname = process.env.PUBLIC_URL + '/';
+}
+
 worker.start({
-  waitUntilReady: true,
-  serviceWorker: { url: `${process.env.PUBLIC_URL}/mockServiceWorker.js` },
+  serviceWorker: {
+    url: `${process.env.PUBLIC_URL}/mockServiceWorker.js`,
+  },
 });
 
 ReactDOM.render(
