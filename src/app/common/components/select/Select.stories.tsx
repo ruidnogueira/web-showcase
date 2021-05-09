ec5dd11391c642bb177824bbf96eb277a3a5e883
Rui Nogueira <ruidnogueira@outlook.com>
2021-05-09 11:38:23 +0100

    chore: add select story and story theme support

--- src/app/common/components/select/Select.stories.tsx
+++ src/app/common/components/select/Select.stories.tsx
@@ -1,5 +1,6 @@
-import { Story, Meta } from '@storybook/react';
+import { Story, Meta, Args } from '@storybook/react';
 import { ColorVariant, ControlSize } from 'app/core/models/styles.model';
+import { Theme } from 'app/core/providers/ThemeProvider';
 import { StorybookVariants } from 'test/storybook.helper';
 import { Select, SelectProps } from './Select';
 
@@ -20,20 +21,35 @@ export default {
 } as Meta<SelectProps<any>>;
 
 const colorVariants = [undefined, ...Object.values(ColorVariant)];
-const Template: Story<SelectProps<any>> = (args) => (
-  <StorybookVariants>
-    {colorVariants.map((variant) => (
-      <Select {...args} key={variant ?? 'undefined'} variant={variant}>
-        <Select.Option value="john">John</Select.Option>
-        <Select.Option value="ann">Ann</Select.Option>
-        <Select.Option value="davis">Davis</Select.Option>
-      </Select>
-    ))}
-  </StorybookVariants>
-);
+const Template: Story<SelectProps<any>> = (args, { globals }: { globals?: Args }) => {
+  const theme: Theme = globals?.theme;
+
+  return (
+    <StorybookVariants>
+      {colorVariants.map((variant) => (
+        <Select
+          {...args}
+          key={variant ?? 'undefined'}
+          variant={variant}
+          dropdownClassName={'theme--' + theme}
+        >
+          <Select.Option value="john">John</Select.Option>
+          <Select.Option value="ann">Ann</Select.Option>
+          <Select.Option value="davis">Davis</Select.Option>
+        </Select>
+      ))}
+    </StorybookVariants>
+  );
+};
 
 export const Default = Template.bind({});
 Default.args = {};
+
+export const Open = Template.bind({});
+Open.args = {
+  open: true,
+  value: 'ann',
+};
 
 export const Disabled = Template.bind({});
 Disabled.args = {
--- src/app/common/components/select/Select.test.tsx
+++ src/app/common/components/select/Select.test.tsx
@@ -1,8 +1,9 @@
 import { render } from '@testing-library/react';
-import Story, { Default, Disabled, Small } from './Select.stories';
+import Story, { Default, Open, Disabled, Small } from './Select.stories';
 
 test.each([
   ['Default', Default],
+  ['Open', Open],
   ['Disabled', Disabled],
   ['Small', Small],
 ])('renders %s story', (_, Component) => {
