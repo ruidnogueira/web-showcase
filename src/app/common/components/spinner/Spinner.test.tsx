d2529bf6a86774a9704667ee9ac75175e0ef292c
Rui Nogueira <ruidnogueira@outlook.com>
2021-05-13 01:58:50 +0100

    fix: add missing providers spinner test

--- src/app/common/components/spinner/Spinner.test.tsx
+++ src/app/common/components/spinner/Spinner.test.tsx
@@ -1,13 +1,13 @@
-import { render } from '@testing-library/react';
 import { createI18nMock } from 'mocks/i18n.mock';
 import { I18nextProvider } from 'react-i18next';
+import { renderWithProviders } from 'test/component.helper';
 import Story, { AcessibilityAlert, Default } from './Spinner.stories';
 
 test.each([
   ['Default', Default],
   ['AcessibilityAlert', AcessibilityAlert],
 ])('renders %s story', (_, Component) => {
-  render(
+  renderWithProviders(
     <I18nextProvider i18n={createI18nMock()}>
       <Component {...Story.args} {...Component.args} />
     </I18nextProvider>
