ac9a0b390b5d9672e6c5e76d6677391576213ef1
Rui Nogueira <ruidnogueira@outlook.com>
2021-08-17 12:05:41 +0100

    chore: add auth tests

--- e2e/tests/login.test.ts
+++ e2e/tests/login.test.ts
@@ -1,6 +1,26 @@
 import { test, expect } from '@playwright/test';
+import { mockAuthToken, mockUserEmail, mockUserPassword } from '../../src/mocks/model/user.mock';
+import { delay } from '../utils/promise';
+import { getDocument, queries } from 'playwright-testing-library';
 
-test('visits app', async ({ page }) => {
+const { findByRole, findByLabelText } = queries;
+
+test('logs in', async ({ page }) => {
+  await page.route('**/auth-token', async (route) => {
+    await delay(1000);
+    return route.fulfill({
+      status: 200,
+      body: mockAuthToken(),
+    });
+  });
+
   await page.goto('/');
-  expect(await page.title()).toBe('Web Showcase');
+  const document = await getDocument(page);
+
+  await (await findByLabelText(document, /Email/i)).type(mockUserEmail());
+  await (await findByLabelText(document, /Password/i)).type(mockUserPassword());
+  await (await findByRole(document, 'button', { name: /Log in/i })).click();
+
+  expect(await findByRole(document, 'button', { name: /loading/i })).toBeDefined();
+  expect(await findByRole(document, 'button', { name: /Log out/i })).toBeDefined();
 });
